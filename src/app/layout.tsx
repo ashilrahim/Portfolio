198add50851e32ad140340f2a00092195c30510b
ashil rahim <ashilrahim2@gmail.com>
2025-11-07 12:14:17 +0530

    added html and analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ import {
 } from "@once-ui-system/core";
 import { Footer, Header, RouteGuard, Providers } from "@/components";
 import { baseURL, effects, fonts, style, dataStyle, about } from "@/resources";
+import { Analytics } from "@vercel/analytics/next"
 
 
 export async function generateMetadata() {
@@ -163,7 +164,7 @@ export default async function RootLayout({
           </Flex>
           <Footer />
 
-
+          <Analytics />
         </Column>
       </Providers>
     </Flex>
